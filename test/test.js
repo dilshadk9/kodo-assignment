4e2321ccd80bb6560671615db553c801bb0a842a
Dilshad Khan <dilshad.khan.in@gmail.com>
2022-05-08 22:04:10 +0530

    Added more test cases

--- test/test.js
+++ test/test.js
@@ -4,9 +4,35 @@ const expect = chai.expect;
 const app = require("../app");
 
 describe("Restful API", function () {
-  it("should return 200 OK with several posts", async function () {
+  it("should return 200 OK with all the posts", async function () {
     const response = await request(app)
       .get("/api/posts")
+      .expect(200)
+      .expect("Content-Type", /json/);
+
+    const posts = response.body;
+    expect(posts).to.be.an("array");
+    expect(posts).length.to.be.greaterThan(0);
+  });
+});
+
+describe("Restful API", function () {
+  it("should return 200 OK with all posts", async function () {
+    const response = await request(app)
+      .get("/api/posts/searchRedis")
+      .expect(200)
+      .expect("Content-Type", /json/);
+
+    const posts = response.body;
+    expect(posts).to.be.an("array");
+    expect(posts).length.to.be.greaterThan(0);
+  });
+});
+
+describe("Restful API", function () {
+  it("should return 200 OK with searched query result", async function () {
+    const response = await request(app)
+      .get("/api/posts/searchRedis?q=dilshad")
       .expect(200)
       .expect("Content-Type", /json/);
 
